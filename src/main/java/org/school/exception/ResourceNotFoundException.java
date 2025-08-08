67522fcae6379669c04025000cdd2fea20d485cd
Abdoul Karim <abdoul625karim@gmail.com>
2025-08-08 10:42:47 +0000

    Création de l'exception pour ressources non trouvée

---
+++ src/main/java/org/school/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package org.school.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
