f0572f019d3f3e655d818417589f064a130b468e
Abdoul Karim <abdoul625karim@gmail.com>
2025-08-08 11:50:21 +0000

    feat: Implémentation du CRUD pour l'entité "Grade"

---
+++ src/main/java/org/school/service/GradeService.java
@@ -0,0 +1,48 @@
+package org.school.service;
+
+import lombok.AllArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+import org.school.dto.GradeRequestDTO;
+import org.school.dto.GradeResponseDTO;
+import org.school.entity.Grade;
+import org.school.exception.ResourceNotFoundException;
+import org.school.mapper.GradeMapper;
+import org.school.repository.GradeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+@DynamicUpdate
+public class GradeService {
+
+    GradeRepository gradeRepository;
+
+    public List<GradeResponseDTO> getAllGrade() {
+        return gradeRepository.findAll().stream().map(GradeMapper::toDTO).toList();
+    }
+
+    public GradeResponseDTO getGradeById(Long id) {
+        Optional<Grade> optionalGrade = gradeRepository.findById(id);
+        if (optionalGrade.isEmpty()) {
+            throw new ResourceNotFoundException("Le niveau n'existe pas");
+        }
+        return GradeMapper.toDTO(optionalGrade.get());
+    }
+
+    public void createGrade(GradeRequestDTO gradeRequestDTO) {
+        gradeRepository.save(GradeMapper.toEntity(gradeRequestDTO));
+    }
+
+    public void updateGrade(Long id, GradeRequestDTO gradeRequestDTO) {
+        Grade grade = gradeRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Le niveau n'existe pas"));
+        grade.setName(gradeRequestDTO.name());
+        gradeRepository.save(grade);
+    }
+
+    public void deleteGrade(Long id) {
+        gradeRepository.deleteById(id);
+    }
+}
