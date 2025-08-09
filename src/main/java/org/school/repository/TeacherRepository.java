1f1cd788c8ebf81833bb0508b7d6eee1d37eb1e9
Abdoul Karim <abdoul625karim@gmail.com>
2025-08-09 18:51:52 +0000

    feat: Création du repository de "Teacher"

---
+++ src/main/java/org/school/repository/TeacherRepository.java
@@ -0,0 +1,9 @@
+package org.school.repository;
+
+import org.school.entity.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+}
