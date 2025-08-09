c101429e1409ace9a7979aaa1fb068fd2e25bcfe
Abdoul Karim <abdoul625karim@gmail.com>
2025-08-09 00:33:11 +0000

    feat: Mis en plage de la gestion des erreurs pour "MethodArgumentTypeMismatchException" et "Exception"

--- src/main/java/org/school/exception/GlobalExceptionHandler.java
+++ src/main/java/org/school/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
 
@@ -17,5 +18,23 @@ public class GlobalExceptionHandler {
         formatedMessageError.setCode(404);
         formatedMessageError.setTimestamp(LocalDateTime.now());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(formatedMessageError);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<FormatedMessageError> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        FormatedMessageError formatedMessageError = new FormatedMessageError();
+        formatedMessageError.setMessage(ex.getMessage());
+        formatedMessageError.setCode(400);
+        formatedMessageError.setTimestamp(LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(formatedMessageError);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<FormatedMessageError> handleException(Exception ex) {
+        FormatedMessageError formatedMessageError = new FormatedMessageError();
+        formatedMessageError.setMessage(ex.getMessage());
+        formatedMessageError.setCode(500);
+        formatedMessageError.setTimestamp(LocalDateTime.now());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(formatedMessageError);
     }
 }
