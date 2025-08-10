6149ae3fe262be801b00bd0a0da80322a8646371
Abdoul Karim <abdoul625karim@gmail.com>
2025-08-10 20:47:43 +0000

    feat: Création du repository de "Subject"

---
+++ src/main/java/org/school/repository/SubjectRepository.java
@@ -0,0 +1,9 @@
+package org.school.repository;
+
+import org.school.entity.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+}
