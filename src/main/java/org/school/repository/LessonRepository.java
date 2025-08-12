60cb42fd8dd80e7068a94aaa65eb05a4c2df9c00
Abdoul Karim <abdoul625karim@gmail.com>
2025-08-12 14:39:12 +0000

    feat: Création du Mapper pour l'entité "Lesson"

---
+++ src/main/java/org/school/repository/LessonRepository.java
@@ -0,0 +1,7 @@
+package org.school.repository;
+
+import org.school.entity.Lesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LessonRepository extends JpaRepository<Lesson, Long> {
+}
