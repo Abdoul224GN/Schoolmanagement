31e077a4b9bc87680d0fb38c07b8c9a2b6c7cfbd
Abdoul Karim <abdoul625karim@gmail.com>
2025-08-14 01:02:17 +0000

    feat: Création du Repository

---
+++ src/main/java/org/school/repository/ClasseRepository.java
@@ -0,0 +1,9 @@
+package org.school.repository;
+
+import org.school.entity.Classe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClasseRepository extends JpaRepository<Classe, Long> {
+}
